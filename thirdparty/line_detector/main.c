915b3ed176bd8b8451fb104ef4b185a0231e9d2b
Chung-Ta Ku <ctku@umd.edu>
2012-08-07 18:22:08 -0400

    Output image with detected lines into "output.jpg" instead.

--- thirdparty/line_detector/main.c
+++ thirdparty/line_detector/main.c
@@ -10,61 +10,63 @@
 #include "extractSegments.h"
 
 int main (int argc, const char * argv[]) {
-   
+
 	IplImage *imagen, *imagen_GRAY, *img_color;
 	tpRectas rectas;
 	int num;
-	
+
 	if (argc!=3) {
 		printf("Incorrect use!\n Correct use: <Program_name> <image_name> <min_pixel/straight>\n");
 		exit(-1);
-	
+
 	}
 
 	imagen=cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
-	
+
 	if (imagen == 0){
 		printf("ERROR to load the image: %s\n",argv[1]);
 		exit(-1);
 	}
-	
-	
+
+
 	imagen_GRAY=cvCreateImage(cvGetSize(imagen),8, 1);
 	cvCvtColor(imagen, imagen_GRAY, CV_RGB2GRAY);
-	
+
 	//reserve memory
 	rectas.numRectas=0;
 	rectas.maximo=MAX_RECTAS;
-	
+
 	rectas.rectas=(tpRecta *)malloc(rectas.maximo*sizeof(tpRecta));
-	
+
 	num=atoi(argv[2]);
 	extractSegments(imagen_GRAY, &rectas, num);
 
-//gui part is not needed
-#if 0		
 	img_color=cvCreateImage(cvGetSize(imagen),8, 3);
 	cvCvtColor(imagen_GRAY, img_color, CV_GRAY2RGB);
-	
-	showSegments(rectas,img_color);
-	
-	//show the original image and the segments detected	
+
+	showSegments(rectas, img_color);
+
+	//pop-up gui is not needed, output into file instead
+#if 1
+	//show the original image and the segments detected in "output.jpg"
+	cvSaveImage("output.jpg", img_color, 0);
+#else
+	//show the original image and the segments detected
 	cvNamedWindow("Input image",CV_WINDOW_AUTOSIZE);
 	cvShowImage("Input image", imagen);
-	
+
 	cvNamedWindow("segments detected",CV_WINDOW_AUTOSIZE);
 	cvShowImage("segments detected", img_color);
-	
 
 	cvWaitKey(0);
 	cvDestroyAllWindows();
-	
+
 	//free memory
 	cvReleaseImage(&imagen);
-	cvReleaseImage(&img_color);
-#endif
 	cvReleaseImage(&imagen_GRAY);
+#endif
+	cvReleaseImage(&img_color);
 	free(rectas.rectas);
-	
+
     return 0;
 }
